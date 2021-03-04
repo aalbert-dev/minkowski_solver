cc0dcf70ee7047d4233b7ba69d9b4b2ef2933ee5
Arjun Albert <34843324+aalbert-dev@users.noreply.github.com>
2021-03-04 01:35:07 -0500

    Simple 2d polygon set dilation

--- set_dilation/include/minkowski_addition.hpp
+++ set_dilation/include/minkowski_addition.hpp
@@ -1,13 +1,43 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include <graphics.h>
+#include <sstream>
 
 using namespace std;
 
 struct Point
 {
   float x, y;
+  Point(){};
   Point(float x, float y) : x(x), y(y){};
   bool operator==(const Point &other) { return x == other.x && y == other.y; }
-  Point(){};
+  Point operator+(const Point &other) { return Point(x + other.x, y + other.y); }
+  Point operator-(const Point &other) { return Point(x - other.x, y - other.y); }
 };
+
+vector<Point> compute_sum(vector<Point> a, vector<Point> b)
+{
+  vector<Point> c;
+  for (int i = 0; i < a.size(); i++)
+  {
+    for (int j = 0; j < b.size(); j++)
+    {
+      Point curr_a = a.at(i);
+      Point curr_b = b.at(j);
+      Point curr_c = curr_a + curr_b;
+      c.push_back(curr_c);
+    }
+  }
+  return c;
+}
+
+void write_results(vector<Point> result)
+{
+  ofstream results_file("results.txt");
+  for (int i = 0; i < result.size(); i++)
+  {
+    Point curr_point = result.at(i);
+    results_file << curr_point.x << ", " << curr_point.y << "\n";
+  }
+  results_file.close();
+}
--- set_dilation/src/minkowski_addition.cpp
+++ set_dilation/src/minkowski_addition.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-    vector<Point> vec1;
-    vec1.push_back(Point(0, 1));
+    vector<Point> triangle_1;
+    triangle_1.push_back(Point(1, 0));
+    triangle_1.push_back(Point(0, 1));
+    triangle_1.push_back(Point(0, -1));
+
+    vector<Point> triangle_2;
+    triangle_2.push_back(Point(0, 0));
+    triangle_2.push_back(Point(1, 1));
+    triangle_2.push_back(Point(1, -1));
+
+    vector<Point> result = compute_sum(triangle_1, triangle_2);
+    write_results(result);
     return 1;
 }
